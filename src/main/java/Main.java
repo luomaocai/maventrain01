cab64cdd44512bf383193a464208eee72bce54b8
maocai <luomaocai@yeah.net>
2019-07-28 23:02:35 +0800

    Maven简单练习，主要用于初步理解Maven，本项目通过构建JavaSE小项目进行实践验证。

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+import org.apache.commons.lang3.StringUtils;
+
+public class Main {
+    public static void main(String[] args){
+        System.out.println("您好");
+        System.out.println(StringUtils.isBlank(""));
+    }
+}
