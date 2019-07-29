b990e55176a2ba4026e653d6b45b1c6fa7508658
maocai <luomaocai@yeah.net>
2019-07-29 14:26:35 +0800

    Maven新增注入lombok

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,18 @@
+import dto.PersonDto;
 import org.apache.commons.lang3.StringUtils;
 
 public class Main {
     public static void main(String[] args){
         System.out.println("您好");
         System.out.println(StringUtils.isBlank(""));
+
+        /*以下测试lombok*/
+        PersonDto maocai = new PersonDto();
+        maocai.setName("冒菜");
+        maocai.setAge(18);
+        System.out.println(maocai);
+
+        PersonDto meichen = new PersonDto("美晨",18);
+        System.out.println(meichen.toString());
     }
 }
--- src/main/java/dto/PersonDto.java
+++ src/main/java/dto/PersonDto.java
@@ -1,9 +1,13 @@
 package dto;
 
-import lombok.Data;
-import lombok.Setter;
+import lombok.*;
 
+//自动生成类所有属性的setter/getter,equals,canEqual,hashCode,toString方法,如为final属性,则不会为该属性生成setter方法
 @Data
+//自动生成无参构造方法
+@NoArgsConstructor(access = AccessLevel.PUBLIC)
+//自动生成全参构造方法,将所有成员属性作为参数
+@AllArgsConstructor(access = AccessLevel.PUBLIC)
 public class PersonDto {
     @Setter
     private String name;
