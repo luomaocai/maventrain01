750633634878c0257a7e4f7fbf35984b09f576c5
maocai <luomaocai@yeah.net>
2019-07-30 19:45:14 +0800

    学习log4j与slf4j

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,33 @@
 import dto.PersonDto;
 import dto.StudentDto;
 import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.logging.Level;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("您好");
-        System.out.println(StringUtils.isBlank(""));
+    private static Logger logger = LoggerFactory.getLogger(Main.class);
 
+    public static void main(String[] args) {
+        logger.info("大家好,欢迎使用maventrain01,现在正在使用log4j打印日志");
+
+        testLof4j();
         testLombok();
+    }
+
+    /** 
+    * @Description:
+    * @Param:  
+    * @return:  
+    * @Author: Maocai.Luo
+    * @Date: 2019/7/30 
+    */ 
+    public static void testLof4j(){
+        logger.info("info级别");
+        logger.debug("debug级别");
+        logger.warn("warn级别");
+        logger.error("error级别");
     }
 
     /**
